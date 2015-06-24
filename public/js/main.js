026fbd332ec3373b3f2f9fae2f5694a70e1f76dc
TX-DSS <195807874@qq.com>
2015-06-24 17:00:57 +0800

    dev admin spot

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,44 @@
+var testGuidepost = [
+	{
+		key: "A",
+		test: "支持"
+	}, {
+		key: "B",
+		test: "反对"
+	}
+]
 
+var Class = {
+    create : function() {
+        return function() {
+            this.initialize.apply(this, arguments);
+        }
+    }
+}
+
+var guidepostEditor = Class.create();
+GuidepostEditor.prototype = {
+    initialize: function(options) {
+        this.SetOptions(options);
+
+    },
+    SetOptions: function(options) {
+        this.options = {
+            domId: '',
+            mode: 'edit',
+            data: [{
+                key: 'A',
+                text: '' 
+            }, {
+                key: 'B',
+                text: '' 
+            }]
+        };
+        $.extend(this.options, options || {});
+    },
+    loadData: function(dat) {
+        if ( !this.options.domId ) return;
+        $('#'+this.options.domId).empty();
+        
+    }
+}

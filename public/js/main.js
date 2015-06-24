97c058b42eb7d9aa30a3b1f8ccba7e00e1261192
Tang Xiang <195807874@qq.com>
2015-06-24 22:29:36 +0800

    dev admin spot page

--- public/js/main.js
+++ public/js/main.js
@@ -16,29 +16,69 @@ var Class = {
     }
 }
 
-var guidepostEditor = Class.create();
+var Bind = function(tar, func) {
+    return function() {
+        func.apply(tar, arguments);
+    }
+}
+
+var GuidepostEditor = Class.create();
 GuidepostEditor.prototype = {
     initialize: function(options) {
         this.SetOptions(options);
+        this.Container = $('#'+this.options.domId)[0];
+        this.GPData = this.options.data;
+        this.GPEditorMap = {};
+        if ( !!this.Container ) {
 
+            this.loadData(this.GPData);
+
+            $(this.Container).bind('click', Bind(this, this.HandleClick));
+        }
     },
     SetOptions: function(options) {
         this.options = {
             domId: '',
             mode: 'edit',
-            data: [{
-                key: 'A',
-                text: '' 
-            }, {
-                key: 'B',
-                text: '' 
-            }]
+            data: []
         };
         $.extend(this.options, options || {});
     },
     loadData: function(dat) {
-        if ( !this.options.domId ) return;
-        $('#'+this.options.domId).empty();
+        if ( !this.Container ) return;
+        $(this.Container).empty();
+
+        this.AddBtn = $('<button type="submit" class="btn btn-default" opt="add">Add</button>');
+        this.AddBtn.appendTo(this.Container);
+
+        this.GPData = dat;
+        if ( !!dat && dat.length>0 ) {
+            for ( var i=0,l=dat.length; i<l; i++ ) {
+                this.AddOption(dat[i]);
+            }
+        }
         
+    },
+    AddOption: function(opt) {
+        this.AddBtn.before('<div class="input-group"><span class="input-group-addon">'+opt.key+'</span><input type="text" class="form-control" placeholder="" value="'+opt.text+'" readonly><span class="input-group-btn"><button class="btn btn-default" type="button" opt="edit">Edit</button><button class="btn btn-danger" type="button" opt="del">Del</button></span></div>');
+
+    },
+    HandleClick: function(e) {
+        console.log(e);
+        console.log(this);
     }
 }
+
+
+$(function(){
+    var editor = new GuidepostEditor({
+        domId: 'inputGuidepost',
+        data: [{
+            key: 'A',
+            text: '123' 
+        }, {
+            key: 'B',
+            text: '321' 
+        }]
+    });
+});

9f1859a2deb7a738d0fc227f730b64ba13ebd249
Tang Xiang <195807874@qq.com>
2015-12-27 21:13:06 +0800

    Revert "Delete credentials.development.js"
    This reverts commit b531636873ff769535b4579f23656e684388c107.

---
+++ credentials.development.js
@@ -0,0 +1,15 @@
+module.exports = {
+    topdomain: 'pow.com',
+    port: '3000',
+    cookieSecret: 'DF21A14S',
+    qqmail: {
+        user: '195807874',
+        password: '0114smtp1988'
+    },
+    mongo: {
+        connectionString: 'mongodb://localhost:27017/powdb'
+    },
+    weixin: {
+        token: "we1x1n"
+    }
+}
